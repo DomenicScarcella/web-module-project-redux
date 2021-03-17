08010cc219fc75562763c3850f05fd610e40f7f0
Domenic Scarcella <ls41513@yahoo.com>
2021-03-16 22:21:57 -0400

    Not MVP, but I don't know what's wrong.  I (still) don't understand JavaScript.

--- src/components/AdditionalFeature.js
+++ src/components/AdditionalFeature.js
@@ -1,13 +1,26 @@
 import React from 'react';
+import { connect } from 'react-redux';
+
+import { addFeature } from '../actions';
 
 const AdditionalFeature = props => {
   return (
     <li>
       {/* Add an onClick that will let you add a feature to your car */}
-      <button className="button">Add</button>
-      {props.feature.name} (+{props.feature.price})
+      <button
+        onClick={() => props.addFeature(props.feature)} className="button">
+        Add
+      </button>
+      {props.feature.name} +{props.feature.price}
     </li>
   );
 };
 
-export default AdditionalFeature;
+const mapStateToProps = (state, props) => {
+  return {
+    ...state,
+    feature: props.feature
+  }
+}
+
+export default connect(mapStateToProps, { addFeature })(AdditionalFeature);
